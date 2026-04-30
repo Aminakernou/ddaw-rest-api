b2a3c092ce3c13330477d4b2883ee2662f95a2f7
Aminakernou <152254536+Aminakernou@users.noreply.github.com>
2026-04-30 11:23:17 +0200

    Update user_routes.py

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -60,12 +60,14 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     if existing:
         raise HTTPException(status_code=400, detail="Email déjà utilisé")
 
-    new_user = User(
-        name=user.name,
-        email=user.email,
-        password=user.password,  
-        role=user.role
-    )
+hashed = bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt())
+
+new_user = User(
+    name=user.name,
+    email=user.email,
+    password=hashed.decode("utf-8"),
+    role=user.role
+)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
